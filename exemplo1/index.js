b81fc4847d73846bc6874c620b51a640db7ab6b7
vitor.gama <vitorggam@hotmail.com>
2023-09-13 09:18:08 -0300

    exemplo2

--- exemplo1/index.js
+++ exemplo1/index.js
@@ -1,6 +1,6 @@
 let aluno = "Maria";
 let nota1 = 9.8;
-let nota2 = 6.4;
+let nota2 = 2.2;
 
 let media = (nota1 + nota2) / 2;
 
@@ -10,13 +10,20 @@ console.log("------------")
 
 
 let dados = {
-    nome : "Joao",
+    aluno : "Joao",
     idade : 20,
     signo : "cabra"
 
 }
 console.log(dados.signo);
 console.table(dados);
+
+if (media >= 7){
+    console.log(`O aluno(a) ${aluno} está aprovado:`)
+} else {
+        console.log(`O aluno(a) ${aluno} está reprovado:`)
+    };
+
 
 
 
---
+++ exemplo2/index.js
@@ -0,0 +1,34 @@
+function soma(valor1, valor2){
+    return valor1 + valor2;    
+}
+
+function multiplica(valor1, valor2){
+    return valor1 * valor2;
+}
+
+let resultadoSoma = soma(10, 50);
+let resultadoMultiplica = multiplica(2, 10);
+
+console.log(`Soma: ${resultadoSoma}`);
+console.log(`multiplicação ${resultadoMultiplica}`);
+
+function dividir(valor1, valor2){
+    return valor1 / valor2;
+}
+
+let resultadoDividir = dividir(40, 20);
+
+function subtrai(valor1, valor2){
+    return valor1 - valor2;
+}
+
+let resultadoSubtrai = subtrai(40, 50);
+
+console.log(`dividir: ${resultadoDividir}`);
+console.log(`subtrai ${resultadoSubtrai}`);
+
+
+
+
+
+
