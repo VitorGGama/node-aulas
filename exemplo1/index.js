ed0a5da9d84c6a8ed7c7ad1b4c366aaa85985f71
vitor.gama <vitorggam@hotmail.com>
2023-09-13 08:44:40 -0300

    Initial commit

---
+++ exemplo1/index.js
@@ -0,0 +1,23 @@
+let aluno = "Maria";
+let nota1 = 9.8;
+let nota2 = 6.4;
+
+let media = (nota1 + nota2) / 2;
+
+console.log(media);   
+console.log("------------")    
+
+
+
+let dados = {
+    nome : "Joao",
+    idade : 20,
+    signo : "cabra"
+
+}
+console.log(dados.signo);
+console.table(dados);
+
+
+
+
